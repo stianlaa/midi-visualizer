b01edbfa0d7a89771cbac5f7d1fbfa1598c585c6
stianlaa <stian.laate@gmail.com>
2022-05-28 10:34:52 +0200

    src/main.rs: imported and tested tungstenite websocket

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,11 @@
 extern crate portmidi as pm;
 
 use std::time::Duration;
-use std::sync::mpsc;
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 use pm::{DeviceInfo, MidiEvent, PortMidi};
+use std::net::TcpListener;
+use tungstenite::{accept, handshake::HandshakeRole, Error, HandshakeError, Message};
 
 // Todo:
 // - Read up on basic svelte setup, and adding websocket svelte
@@ -66,7 +68,7 @@ fn map_event_to_note(event: MidiEvent) -> Note {
     Note { octave, key }
 }
 
-fn start_listener(context: PortMidi, tx: mpsc::Sender<ChannelMessage>) {
+fn start_listener(context: PortMidi, tx_channel: mpsc::Sender<ChannelMessage>) {
     thread::spawn(move || {
         let in_ports = context
             .devices()
@@ -77,7 +79,7 @@ fn start_listener(context: PortMidi, tx: mpsc::Sender<ChannelMessage>) {
         loop {
             for port in &in_ports {
                 if let Ok(Some(events)) = port.read_n(BUF_LEN) {
-                    tx.send(ChannelMessage { device: port.device(), events }).unwrap();
+                    tx_channel.send(ChannelMessage { device: port.device(), events }).unwrap();
                 }
             }
             thread::sleep(TIMEOUT);
@@ -85,18 +87,86 @@ fn start_listener(context: PortMidi, tx: mpsc::Sender<ChannelMessage>) {
     });
 }
 
+// TODO wrong mapping, already closed is incorrect
+fn must_not_block<Role: HandshakeRole>(err: HandshakeError<Role>) -> Error {
+    match err {
+        HandshakeError::Interrupted(_) => Error::AlreadyClosed,
+        HandshakeError::Failure(f) => f,
+    }
+}
+
+fn start_websocket_server(rx_channel: mpsc::Receiver<ChannelMessage>) {
+    // TODO perhaps have to use arc, because rx_channel is moved into separate thread
+    let rx_arc = Arc::new(Mutex::new(rx_channel));
+    thread::spawn(move || {
+        let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+        // TODO should be made indepndent of incoming messages, should have a (list?) of clients, and send to those
+        loop {
+            for stream_result in server.incoming() {
+                match stream_result {
+                    Ok(stream) => {
+                        let rx_channel_lock = rx_arc.lock();
+
+                        // Should keep list of connected clients
+
+                        // Should publish to all those clients
+
+                        // TODO first test looping response, after client connect
+                        let mut inc = 0;
+
+                        let mut socket = accept(stream).map_err(must_not_block).expect("Couldn't expect websocket");
+                        println!("Waiting for client connection");
+                        loop {
+                            /*
+                            match socket.read_message().expect("Can't ready message") {
+                                received_message @ Message::Text(_) => {
+                                    //let channel_message = rx_channel_lock.recv().unwrap();
+                                    // for midi_event in channel_message.events {
+                                    //      let message_string = format!("Played note: {:?}", map_event_to_note(midi_event));
+                                    // }
+
+                                    println!("Received from client: {:?}", received_message);
+                                    let test_message: String = String::from("Placeholder response");
+                                    socket.write_message(Message::Text(test_message));
+                                }
+                                Message::Ping(_) | Message::Pong(_) | Message::Close(_) | Message::Frame(_) | Message::Binary(_) => {}
+                            }
+
+                             */
+
+                            inc += 1;
+                            thread::sleep(Duration::from_secs(2));
+                            let test_message = String::from(format!("Placeholder response: {:?}", inc));
+                            socket.write_message(Message::Text(test_message))
+                        }
+                    }
+                    Err(err) => println!("Stream error: {err:?}"),
+                }
+
+
+                // TODO improve
+                // let message = rx_channel.recv().unwrap();
+                //for event in message.events {
+                //    // println!("[{}] {:?}", message.device, event);
+                //    println!("{:?}", map_event_to_note(event));
+                //}
+            }
+        }
+    });
+}
+
 fn main() {
     let context = pm::PortMidi::new().unwrap();
-    let (tx, rx) = mpsc::channel::<ChannelMessage>();
+    let (tx_channel, rx_channel) = mpsc::channel::<ChannelMessage>();
 
     // Create listener thread, listening to all connected midi devices
-    start_listener(context, tx);
+    start_listener(context, tx_channel);
+
+    // Create websocket server thread, forwarding midi messages
+    start_websocket_server(rx_channel);
 
     loop {
-        let message = rx.recv().unwrap();
-        for event in message.events {
-            // println!("[{}] {:?}", message.device, event);
-            println!("{:?}", map_event_to_note(event));
-        }
+        println!("Running!");
+        thread::sleep(Duration::from_secs(2));
     }
 }
