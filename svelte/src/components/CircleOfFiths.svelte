c0bfce164112640f727c5817890702ddc198e260
stianlaa <stian.laate@gmail.com>
2022-05-28 13:06:09 +0200

    svelte/src/components/CircleOfFifths: Added websocket client

--- svelte/src/components/CircleOfFiths.svelte
+++ svelte/src/components/CircleOfFiths.svelte
@@ -1,8 +1,24 @@
 <script>
+    import { onMount } from "svelte";
+    // import { webSocketClient } from "websocket";
+
+    let socket;
+    onMount(() => {
+        socket = new WebSocket("ws://localhost:9001/");
+        socket.addEventListener("open", () => {
+            console.log("Websocket opened");
+        });
+
+        socket.onmessage = function (e) {
+            if (typeof e.data === "string") {
+                console.log("Received: '" + e.data + "'");
+            }
+        };
+    });
 </script>
 
 <main>
-	<h1>This is the circle-of-fifths component!</h1>
+    <h1>This is the circle-of-fifths component!</h1>
 </main>
 
 <style>
